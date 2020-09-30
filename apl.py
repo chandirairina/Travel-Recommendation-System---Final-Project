3bdcbe1c5a53d0c1807967b2691961207cad03c9
chandirairina <chirinajap@gmail.aom>
2020-09-30 20:20:09 +0700

    inputan destination 2 kata, hotel data pake Rp

--- apl.py
+++ apl.py
@@ -52,7 +52,6 @@ def home():
     df2=pd.read_csv("dataset_recsys2_engineered_droptur.csv")
     df3=df1.merge(df2, on="Travel Destination")[["Travel Destination","Spot / Vendor","Type","Hotel Best Deal", "Flight Best Deal"]].head(25)
     
-
     return render_template('home.html', drop_places=list_places, list_activity=list_activity, list_option=list_option, data=df3.to_html())
 
 @app.route('/recommendation', methods=['POST'])
@@ -63,10 +62,8 @@ def hasil():
         DataUser = request.form
         place=DataUser['destination']
 
-        print(place)
         df1=pd.read_csv("dataset_recsys1_engineered.csv")
         df2=pd.read_csv("dataset_recsys2_engineered_droptur.csv")
-        df4=pd.read_csv("datasetindo_hotel_flight.csv")
         
         list_places=df1["Travel Destination"].unique()
         list_places.sort()
@@ -93,84 +90,26 @@ def hasil():
                     recomm_id.append (i)
             
             recomm_data=[]
+            # hotel_price=[]
+            # hotel_link=[]
+            # flight_price=[]
+            # flight_link=[]
             for j in recomm_id[0:10]:
                 if (df1.loc[j]["Travel Destination"])!=place:
                     recomm_data.append(df1.loc[j]["Travel Destination"])
+                    # hotel_price.append(df1.loc[j]["Hotel Best Deal"])
+                    # hotel_link.append(df1.loc[j]["Hotel Link"])
+                    # flight_price.append(df1.loc[j]["Flight Best Deal"])
+                    # flight_link.append(df1.loc[j]["Hotel Link"])
+
 
             return recomm_data
-
-        user_type=""
-        
-        for i in list_activity:
-            user_type+=(DataUser[i]+" ")
-
-        user_data={"Spot / Vendor":"user",
-            "Type": user_type}
-
-        df2=df2.append(user_data, ignore_index=True)
-
-        cv_spot = CountVectorizer()
-        cv_spot_result = cv_spot.fit_transform(df2['Type'])
-
-        cv_spot.get_feature_names()
-
-        cv_df_spot = pd.DataFrame(cv_spot_result.todense(),columns= cv_spot.get_feature_names(),index=df2['Spot / Vendor'])
-        cv_df_spot
-
-        cos_sin_spot = cosine_similarity(cv_spot_result)
-
-        def get_recomendation_spot(place):
-            index_to_search_spot = df2[df2['Spot / Vendor'] == "user"].index[0]
-            place_similar_spot = pd.Series(cos_sin_spot[index_to_search_spot])
-            index_similar_spot = place_similar_spot.sort_values(ascending=False).index
-            
-            similar_spot_idx=[]
-            for i in index_similar_spot:
-                if i != 12423:
-                    similar_spot_idx.append(i)
-                
-            recom_destination=[]
-            spot_in_destination=[]
-            link_spot=[]
-            recom_hotel_best=[]
-            recom_hotel_link=[]
-            recom_flight_best=[]
-            recom_flight_link=[]
-            
-            for i in get_recomendation(place):
-                recom_destination.append(i)
-                spot_per_destination=0
-                recom_spot=[]
-                recom_spot_link=[]
-                for j in similar_spot_idx:
-                    if df2.loc[j]["Travel Destination"]==i:
-                        if spot_per_destination<5:
-                            recom_spot.append(df2.loc[j]["Spot / Vendor"])
-                            recom_spot_link.append(df2.loc[j]["Link"])
-                            spot_per_destination+=1
-                spot_in_destination.append(recom_spot)
-                link_spot.append(recom_spot_link)
-                recom_hotel_best.append(df4.loc[df4[df4["Travel Destination"]==i].index[0]]["Hotel Best Deal"])
-                recom_hotel_link.append(df1.loc[df1[df1["Travel Destination"]==i].index[0]]["Hotel Link"])
-                recom_flight_best.append(df1.loc[df1[df1["Travel Destination"]==i].index[0]]["Flight Best Deal"])
-                recom_flight_link.append(df1.loc[df1[df1["Travel Destination"]==i].index[0]]["Flight Link"])
-
-
-            return recom_destination, spot_in_destination, link_spot, recom_hotel_best, recom_hotel_link, recom_flight_best, recom_flight_link
-
-    a,b,c,d,e,f,g=get_recomendation_spot(place)
-
     
+    output=get_recomendation(place)
 
     return render_template('recommend.html', 
     input=DataUser, 
-    output_destination=a, 
-    output_spot=b, 
-    output_link_spot=c, 
-    output_hotel=d, 
-    output_link_hotel=e, 
-    output_flight=f, 
-    output_link_flight=g)
+    output=output)
 
 if __name__ == "__main__":
 
